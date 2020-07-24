9f75f02c45b5d339ebf7a804052e2e06a71a4cac
Maxxxxder <maxder135@gmail.com>
2020-07-24 13:08:26 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+ alert('Spoilers!');
+
+ $('.spoiler').hide();
+ $('.goback').hide();
+ $('.spoilerButton2').hide();
+ $('.athrey').hide();
+
+ $('.spoilerButton').on('click', function() {
+   $('.spoilerButton').hide();
+   $('.spoiler').slideDown(2000);
+   $('.goback').slideDown(2000);
+ });
+
+ $('.goback').on('click', function() {
+   $('.spoiler').hide();
+   $('.goback').hide();
+   $('.athrey').hide();
+   $('.spoilerButton').slideDown(2000);
+   $('.spoilerButton2').slideDown(2000);
+ });
+
+ $('.spoilerButton2').on('click', function() {
+   $('.spoilerButton').hide();
+   $('.spoilerButton2').hide();
+   $('.athrey').slideDown(2000);
+   $('.goback').slideDown(2000);
+ });
